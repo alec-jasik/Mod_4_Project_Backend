8b36823c5c2710da7810675254288ad0ea0e511c
SaintHugo <hugor37@gmail.com>
2021-01-08 08:08:50 -0600

    updated show for teams

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::TeamsController < ApplicationController
         teams = Team.all
         render json: teams, include: :players
     end
+
+    def show
+        team = Team.find(params[:id])
+        render json: team, include: :players
+    end
 end
